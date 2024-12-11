b51c1c5e1e733eaebc17c6c85479bb09e7634703
nyrtsk70 <nyrtsk70@gmail.com>
2024-12-11 18:51:50 +0700

    yyyy

--- Source1.cpp
+++ Source1.cpp
@@ -1,7 +1,6 @@
 int main()
 {
 	setlocale(LC_ALL, "Ru");
-	char x;
 	char y;
 	std::cout << "Введите первое 8-ми битное число:///// ";
 	std::cin >> x;
@@ -10,7 +9,7 @@ int main()
 	x |= (1 << 1);
 	y &= ~(1 << 3);
 	y &= ~(1 << 4);
-	unsigned char shifted_x = x << 5;
+	unsigned char shifted_x = x << 5;//меняююююююююююю
 	unsigned char shifted_y = y >> 4;
 	std::cout << "Исходное значения: " << "x = " << (int)x << ", y = " << (int)y << std::endl;
 	std::cout << "Преобразованные значения: " << "x = " << (int)shifted_x << ", y = " << (int)shifted_y << std::endl;//ee
